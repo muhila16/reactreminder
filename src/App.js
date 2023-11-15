9c0e53f6e3c6f7557797647aed00555d8c72264b
muhila16 <muhila.s2021@vitstudent.ac.in>
2023-11-15 18:52:30 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Reminder from './Reminder';
+import './App.css'
+import Animation from './Animation';
+
+
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <Reminder />
+      <Animation />
+      <div id="stars"></div>
+      <div id="stars2"></div>
+      <div id="stars3"></div>
+      
     </div>
   );
 }
 
 export default App;
+
